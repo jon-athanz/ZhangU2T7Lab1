70bb2783c540fae112bf139e41bb72632c99ec2e
Jonathan <jonathanz28@nycstudents.net>
2022-10-12 09:30:22 -0400

    ZhangU2T7Lab1

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class LabRunner {
+public class LabRunner {
+    public static void main(String[] args) {
+        String myString = "AP CSA is awesome!";
+        String s = myString.substring(4,5);
+        System.out.println(s);
+
+        String awe = myString.substring(10,18);
+        System.out.println(awe);
+    }
 }
